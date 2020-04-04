4a7b5c43568eeeef40d755fac0e798650351de31
Orlando Gomes <orlandosjm@yahoo.com.br>
2020-04-04 11:37:49 -0300

    Incluído template de Swagger Documentation

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,11 @@
 import { Router } from 'express';
 
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from '../swagger.json';
+
 const routs = new Router();
+
+routs.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 routs.get('/', (req, res) => {
   return res.json({ msg: 'Hello world' });
