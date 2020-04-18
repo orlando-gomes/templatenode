c2d1398bf394648ebe2b085b999da02d0d623489
Orlando Gomes <orlandosjm@yahoo.com.br>
2020-04-18 11:54:05 -0300

    Incluídas rotas para microserviços

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,17 @@
+const dotenv = require('dotenv');
+
+dotenv.config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+});
+
 module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'gobarber',
+  dialect: process.env.DB_DIALECT || 'postgres',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.BD_PASS,
+  database: process.env.DB_NAME,
+  storage: './__tests__/database.sqlite',
+  logging: false,
   define: {
     timestamps: true,
     underscored: true,
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,24 @@ import { Router } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import * as swaggerDocument from '../swagger.json';
 
+import 'dotenv/config';
+
 const routs = new Router();
 
 routs.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-routs.get('/', (req, res) => {
-  return res.json({ msg: 'Hello world' });
-});
+routs.get(
+  `${process.env.NODE_ENV === 'production' ? process.env.MS : ''}/`,
+  (req, res) => {
+    return res.json({ msg: 'Hello world' });
+  }
+);
+
+routs.get(
+  `${process.env.NODE_ENV === 'production' ? process.env.MS : ''}/users`,
+  (req, res) => {
+    return res.json({ msg: 'get users' });
+  }
+);
 
 export default routs;
